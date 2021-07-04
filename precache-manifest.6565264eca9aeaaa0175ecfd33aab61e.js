62f35cd2916c321511d3385e7d34517af2c79c8a
NEWUSER <abdulla4g3@sasi.ac.in>
2021-07-04 12:15:27 -0700

    Updates

---
+++ precache-manifest.6565264eca9aeaaa0175ecfd33aab61e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc2c827edb0c390679b8",
+    "url": "/avinash/static/css/main.dd66c094.chunk.css"
+  },
+  {
+    "revision": "bc2c827edb0c390679b8",
+    "url": "/avinash/static/js/main.bc2c827e.chunk.js"
+  },
+  {
+    "revision": "f608697f507c53c2a042",
+    "url": "/avinash/static/js/1.f608697f.chunk.js"
+  },
+  {
+    "revision": "99c1a17f49973a44400a",
+    "url": "/avinash/static/js/runtime~main.99c1a17f.js"
+  },
+  {
+    "revision": "6c426583a257819912278cedfdd9badf",
+    "url": "/avinash/static/media/item1.6c426583.jpg"
+  },
+  {
+    "revision": "8779dd15782203466bfb67cac99ef2ec",
+    "url": "/avinash/static/media/item2.8779dd15.jpg"
+  },
+  {
+    "revision": "9bd2546f523e4a6e2515068c65ca441c",
+    "url": "/avinash/static/media/item3.9bd2546f.jpg"
+  },
+  {
+    "revision": "65bc33d70f7f4ca793c5acc24525de9f",
+    "url": "/avinash/static/media/item4.65bc33d7.jpg"
+  },
+  {
+    "revision": "f5fd97b56137c3ed5df0f3f71482c105",
+    "url": "/avinash/static/media/item6.f5fd97b5.jpg"
+  },
+  {
+    "revision": "d2d120f6378ecfc68fd0d193a58b78af",
+    "url": "/avinash/index.html"
+  }
+];
